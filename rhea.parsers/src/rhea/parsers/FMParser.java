eb63a2bdb35ad0c07803350ec15cf8541082fef9
José Miguel Horcas <horcas@lcc.uma.es>
2020-11-16 14:44:29 +0100

    JavaDoc for FMParser interface

--- rhea.parsers/src/rhea/parsers/FMParser.java
+++ rhea.parsers/src/rhea/parsers/FMParser.java
@@ -2,6 +2,17 @@ package rhea.parsers;
 
 import rhea.metamodels.BasicFMs.FeatureModel;
 
+/**
+ * Interface to convert a feature model from a concrete syntax to the abstract syntax of the metamodels.
+ *
+ */
 public interface FMParser {
+	
+	/**
+	 * Parse a feature model in a concrete syntax to the abstract syntax of the metamodels.
+	 * 
+	 * @param filepath		Filepath of the feature model (any type/extension).
+	 * @return				Feature model representation in the abstract syntax.
+	 */
 	public FeatureModel readFeatureModel(String filepath);
 }
