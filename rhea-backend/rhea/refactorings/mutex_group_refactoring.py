b4b47daac6cf19e4d277d66efd6c2a70010937bc
Adriana <anhe2p9@uma.es>
2022-07-13 15:35:06 +0200

    First version with tests

--- rhea-backend/rhea/refactorings/mutex_group_refactoring.py
+++ rhea-backend/rhea/refactorings/mutex_group_refactoring.py
@@ -1,8 +1,9 @@
 from typing import Any
 
-from famapy.metamodels.fm_metamodel.models import FeatureModel, Feature
+from famapy.metamodels.fm_metamodel.models import FeatureModel, Feature, Relation
 
 from rhea.refactorings import Refactoring
+from rhea.refactorings import utils
 
 
 class MutexGroupRefactoring(Refactoring):
@@ -17,4 +18,25 @@ class MutexGroupRefactoring(Refactoring):
 
     @staticmethod
     def transform(model: FeatureModel, instance: Feature) -> FeatureModel:
+        if instance is None:
+            raise Exception(f'There is not feature with name "{instance.name}".')
+        if not instance.is_mutex_group():
+            raise Exception(f'Feature {instance.name} is not a mutex group.')
+    
+        new_name = utils.get_new_feature_name(model, instance.name)
+        f_p = Feature(name=new_name, parent=instance)
+        r_opt = Relation(instance, [f_p], 0, 1)  # optional
+        r_mutex = next((r for r in instance.get_relations() if r.is_mutex()), None)
+        r_mutex.parent = f_p
+        r_mutex.card_min = 1  # xor
+    
+        for child in r_mutex.children:
+            child.parent = f_p
+
+        instance.get_relations().remove(r_mutex)
+
+        # Add relations to features
+        instance.add_relation(r_opt)
+        f_p.add_relation(r_mutex)
         return model
+
