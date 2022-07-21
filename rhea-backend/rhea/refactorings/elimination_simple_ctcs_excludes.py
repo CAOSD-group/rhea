61f0b4be030a1332c1ff971ccdfc438906ce7f07
Adriana <anhe2p9@uma.es>
2022-07-21 15:48:21 +0200

    Elimination of constraints from Feature Trees

---
+++ rhea-backend/rhea/refactorings/elimination_simple_ctcs_excludes.py
@@ -0,0 +1,43 @@
+from ast import Constant
+from typing import Any
+
+from famapy.metamodels.fm_metamodel.models import FeatureModel, Feature, Relation, Constraint
+
+from rhea.refactorings import FMRefactoring
+from rhea.refactorings import utils
+
+
+class EliminationSimpleConstraintsExcludes(FMRefactoring):
+
+    @staticmethod
+    def get_name() -> str:
+        return 'Elimination of Constraints from Feature Trees - Excludes'
+
+    @staticmethod
+    def get_instances(model: FeatureModel) -> list[Feature]:
+        return [f for f in model.get_features() if f.is_mutex_group()]
+
+    @staticmethod
+    def transform(model: FeatureModel, instance: Constraint) -> FeatureModel:
+        if instance is None:
+            raise Exception(f'There is not feature with name "{instance.name}".')
+        if not instance.is_mutex_group():
+            raise Exception(f'Feature {instance.name} is not a mutex group.')
+    
+        new_name = utils.get_new_feature_name(model, instance.name)
+        f_p = Feature(name=new_name, parent=instance, is_abstract=True)
+        r_opt = Relation(instance, [f_p], 0, 1)  # optional
+        r_mutex = next((r for r in instance.get_relations() if r.is_mutex()), None)
+        r_mutex.parent = f_p
+        r_mutex.card_min = 1  # xor
+    
+        for child in r_mutex.children:
+            child.parent = f_p
+
+        instance.get_relations().remove(r_mutex)
+
+        # Add relations to features
+        instance.add_relation(r_opt)
+        f_p.add_relation(r_mutex)
+        return model
+
