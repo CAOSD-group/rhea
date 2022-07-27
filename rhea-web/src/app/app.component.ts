58e6428df2a3936c95a775734f16424aeb6a96ed
sebastian <0619725703@uma.es>
2022-07-27 13:50:31 +0200

    rhea-web

--- rhea-web/src/app/app.component.ts
+++ rhea-web/src/app/app.component.ts
@@ -9,6 +9,7 @@ import{Rama} from 'src/app/components/miarbol/miarbol'
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
 import { FMEditor } from './components/fm-editor/fm-editor.component';
+import { Arbol } from './components/arbol_pruebas/arbol';
 
 
 @Component({
@@ -19,61 +20,75 @@ import { FMEditor } from './components/fm-editor/fm-editor.component';
 export class AppComponent {
   url="http://172.16.51.94:5000"  //servidor 
   articulos: any;        //atributo donde guardo las respuestas
-  documentos:string[]= ['GPL.xml', 'JHipster.uvl', 'MobileMedia.xml', 'Pizzas.uvl', 'TankWar.xml', 'Truck.uvl','WeaFQAs.uvl','Pizzas.json'];
+  documentos:string[]= ['GPL.xml', 'JHipster.uvl', 'MobileMedia.xml', 'Pizzas.uvl', 'TankWar.xml', 'Truck.uvl','WeaFQAs.uvl'];
   item:string ='';       //variable para el titulo 
   declare mijson:JSON    //variable que guarda el objeto tipo JSON 
   rama:Rama =new Rama();    //para poder llamar metodos Rama
   fmeditor:FMEditor =new FMEditor();
   children: Array<AppComponent> =[] ;
-  treeControl = new NestedTreeControl<AppComponent>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<AppComponent>();
+  treeControl = new NestedTreeControl<Arbol>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<Arbol>();
   showFiller = false;   //controla si se ve mas contenido
   istoggle=false;       // controla si el menu esta abierto o no
   title:string ='rhea-web' // evita un error en app.component.spec.ts
+  declare json_nombre:any;
+  declare json_const:any;
+  tree:Array<Arbol> =[new Arbol()]
+
 
   
 constructor(private http: HttpClient) { }  
-ngOnInit() {this.getValues();}
+ngOnInit() {}
 
-getValues(){
+/*getValues(){
   this.http.get(this.url,{responseType:'text'})   //direccion de donde saca los datos 
     .subscribe(resultado => {this.articulos = resultado});
-    this.item=this.articulos;
-    
-}
+    this.item=this.articulos;   
+  }*/
 
 returnValues(texto?:string){
   if(texto==""){texto="te he escuchado"}
   this.http.post(this.url,texto,{responseType:'text'}).subscribe(resultado => {
     this.articulos = resultado;
     this.mijson=this.articulos
-    console.log(this.articulos)
     this.item=texto||"";
-    this.dataSource.data=[]
-    this.dataSource.data=[this.articulos]})
+    this.articulos=JSON.parse(this.articulos)
+    this.actualizar_datos()})
 }
 getArchivo(texto?:string){
   this.http.post(this.url,texto,{responseType:'text'}).subscribe(resultado => {
     this.articulos = resultado;
     this.mijson=this.articulos
     this.item=texto||"";
-    this.dataSource.data=[]
-    this.dataSource.data=[this.articulos]})
-   
+    this.articulos=JSON.parse(this.articulos)
+    console.log(this.articulos)
+    this.json_nombre=this.articulos.features,
+    this.json_const=this.articulos.constraints,
+    console.log(this.json_nombre)
+    console.log(this.json_nombre.type)
+    console.log(this.json_const)
+    this.actualizar_datos() })
 }
-getRama(){
-  this.rama.crearRamaControlada(this.mijson)
-  //this.rama.saveAsProject(this.mijson)
-}
+
 hasChild = (_: number, node: AppComponent) => !!node.children && node.children.length >= 0;
 
-algoFM_editor(){
-  this.fmeditor.nuevovalor2("ejemplo")
+actualizar_datos(){ 
+  this.dataSource.data=[]
+  this.dataSource.data=[this.json_nombre]
+  console.log(this.json_nombre)
+}
+
+crearArbol(){
+  this.tree.splice(0,this.tree.length)
+  this.tree=[new Arbol()]
+  this.tree=this.tree[0].CrearArbol(this.json_nombre)
+  this.tree=this.tree[0].organizarArbol(this.json_nombre,this.tree)
+  console.log(this.tree)
+  this.dataSource.data=[]
+  this.dataSource.data=this.tree
+}
+borrarArbol(){
+  this.dataSource.data=[]
+}
+}
  
-}
-
-
-
-
-
-}
--- rhea-web/src/app/components/arbol_pruebas/arbol.ts
+++ rhea-web/src/app/components/arbol_pruebas/arbol.ts
@@ -1,177 +1,88 @@
-import {Component} from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core'
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { Injectable } from '@angular/core';
+import { NONE_TYPE } from '@angular/compiler';
+import { data, getJSON } from 'jquery';
+import { jsDocComment } from '@angular/compiler';
 
+
+
+
+let Nname:string;
+let Nabstract:boolean;
+let Noptional:boolean;
+let Ntype:string;
+let Nchildren:Array<Arbol>;
+
+
+
+let aux :any=0 // variable auxiliar 
+let arbol :Array<Arbol> =[]
 
 interface FeatureNode {
     name: string;
     children?: FeatureNode[];
 }
 
-const TREE_DATA: FeatureNode[] = [
-    {
-        "name": "truck",
-        "children": [
-          {
-            "name": "weight",
-            "children": [
-              {
-                "name": "lightweight",
-                "children": [
-                  {
-                    "name": "12tons"
-                  },
-                  {
-                    "name": "18tons"
-                  },
-                  {
-                    "name": "20tons"
-                  }
-                ]
-              },
-              {
-                "name": "heavyweight",
-                "children": [
-                  {
-                    "name": "23tons"
-                  },
-                  {
-                    "name": "40tons"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "name": "type",
-            "children": [
-              {
-                "name": "semitrailer"
-              },
-              {
-                "name": "tank"
-              },
-              {
-                "name": "flatbed",
-                "children": [
-                  {
-                    "name": "dumper"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "name": "prueba",
-            "children": [
-              {
-                "name": "prueba1"
-              },
-              {
-                "name": "prueba_hijo",
-                "children": [
-                  {
-                    "name": "ultima"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "name": "engine",
-            "children": [
-              {
-                "name": "160kw"
-              },
-              {
-                "name": "280kw"
-              },
-              {
-                "name": "400kw"
-              }
-            ]
-          },
-          {
-            "name": "cabin",
-            "children": [
-              {
-                "name": "highroof"
-              },
-              {
-                "name": "sleepercabin",
-                "children": [
-                  {
-                    "name": "1bed"
-                  },
-                  {
-                    "name": "2beds"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "name": "axles",
-            "children": [
-              {
-                "name": "count",
-                "children": [
-                  {
-                    "name": "2axles"
-                  },
-                  {
-                    "name": "multiplerearaxles",
-                    "children": [
-                      {
-                        "name": "3axles"
-                      },
-                      {
-                        "name": "4axles"
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                "name": "additionalsteeringaxle"
-              },
-              {
-                "name": "drivetrain",
-                "children": [
-                  {
-                    "name": "1drivenaxle"
-                  },
-                  {
-                    "name": "2drivenaxles"
-                  },
-                  {
-                    "name": "3drivenaxles"
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      },
-  ];
-
-
 @Component({
     selector: 'arbol',
     templateUrl: './arbol.html',
-    styleUrls: ['./arbol.css'],
 })
-export class arbol {
-    treeControl = new NestedTreeControl<FeatureNode>(node => node.children);
-    dataSource = new MatTreeNestedDataSource<FeatureNode>();
+
+ export class Arbol  {
+    abstract:boolean =false;
+    optional:boolean =false;
+    name:string ="";
+    type:string ="";
+    children:Array<Arbol> =[];
+    aux=0
+    treeControl = new NestedTreeControl<Arbol>(node => node.children);
+    dataSource = new MatTreeNestedDataSource<Arbol>();
+    
 
 
     constructor() {
-        this.dataSource.data = TREE_DATA;
+        this.name=Nname;
+        this.abstract=Nabstract;
+        this.optional=Noptional;
+        this.type=Ntype;
+    }
+
+    CrearArbol(valor: any){             // Crea los objetos pero no los introduce como hijos unos del otro
+        Nname=valor.name;
+        Nabstract=valor.abstract;
+        Noptional=valor.optional;
+        Ntype=valor.type;
+        if(valor.children!=undefined){
+        Nchildren=valor.children}
+        else {Nchildren=[] }
+        arbol[arbol.length]=new Arbol()
+        if(valor.children!=undefined){
+        if(Nchildren.length>0){
+            aux=0
+            valor.children.forEach(element => {
+                this.CrearArbol(element);
+                Nchildren.push(element)
+            });
+        }}
+        return arbol
+    }
+    
+    valorHijos(algo:any){
+        Nname=algo.name;
+        Nabstract=algo.abstract;
+        Noptional=algo.optional;
+        Ntype=algo.type;
+        if(algo.children!=undefined){
+            Nchildren=algo.children}
+        else {Nchildren=[] }
+    }
+    organizarArbol(valor: any,lista:Array<Arbol>){  // recorrer los dos archivos y compararlos para meter los hijos
+        
+        return arbol
     }
 
     hasChild = (_: number, node: FeatureNode) => !!node.children && node.children.length >= 0;
-    changeName(new_name:string){
-        
-    }
+    
 }
