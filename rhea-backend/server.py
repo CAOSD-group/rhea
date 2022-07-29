ab0645f309fe673f8db424f77599baf4a7d985be
sebastian <0619725703@uma.es>
2022-07-29 13:30:25 +0200

    python 13:30

--- rhea-backend/server.py
+++ rhea-backend/server.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from cgitb import text
 import json
 from pickle import TRUE
 import re
@@ -8,6 +9,7 @@ from types import NoneType
 from typing import Optional
 from urllib import response
 from xml.etree.ElementTree import tostring
+import os
 
 from flask import Flask, flash, jsonify, render_template, request
 from flask_cors import CORS
@@ -23,7 +25,6 @@ static_url=''
 static_dir = 'template'
 static_folder='web'
 #static_dir = 'web'
-
 
 app = Flask(__name__,static_url_path=static_url,static_folder=static_folder,template_folder=static_dir)
 CORS(app)
@@ -59,14 +60,15 @@ def read_fm_file(filename: str) -> Optional[FeatureModel]:
 
 @app.route('/saveFM', methods=['GET', 'POST'])
 def save_feature_model():
+    
     if request.method == 'GET':
         texto=request.data.decode() 
         return texto
 
     if request.method == 'POST':
         texto=request.data.decode() 
-        #print(request)                      <Request 'http://172.16.51.94:5000/saveFM' [POST]>
-        #print(texto)                         arbol con formato texto
+        print(request.files.values())                      #<Request 'http://172.16.51.94:5000/saveFM' [POST]>
+        #print(texto)                       #  arbol con formato texto
         fm_file = request.from_values(texto)
         #print(fm_file)                   # <Request 'http://localhost/namespace%20Pizza%0A%0Afeatures%0A%09Pizza%20%7Babstract%7D%09%0A%09%09mandatory%0A%09%09%09Topping%09%0A%09%09%09%09or%0A%09%09%09%09%09Salami%0A%09%09%09%09%09Ham%0A%09%09%09%09%09Mozzarella%0A%09%09%09Size%09%0A%09%09%09%09alternative%0A%09%09%09%09%09Normal%0A%09%09%09%09%09Big%0A%09%09%09Dough%09%0A%09%09%09%09alternative%0A%09%09%09%09%09Neapolitan%0A%09%09%09%09%09Sicilian%0A%0A%09%09optional%0A%09%09%09CheesyCrust%0A%0Aconstraints%0A%09CheesyCrust%20%3D%3E%20Big' [GET]>
         print("hola mundo")
