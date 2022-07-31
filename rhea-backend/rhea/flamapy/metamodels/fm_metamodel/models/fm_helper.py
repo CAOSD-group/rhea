9ca7de324f8494ceb57d66f2a357184936291155
José Miguel Horcas <jhorcas@us.es>
2022-07-31 18:55:49 +0200

    Identification of types for complex and simple constraints

--- rhea-backend/rhea/flamapy/metamodels/fm_metamodel/models/fm_helper.py
+++ rhea-backend/rhea/flamapy/metamodels/fm_metamodel/models/fm_helper.py
@@ -12,6 +12,7 @@ class FM(FeatureModel):
     def __init__(self, feature_model: FeatureModel) -> None:
         self._fm: FeatureModel = feature_model
         self._attributes: dict[Feature, list[Attribute]] = {}
+        self._constraints: dict[Constraint, ConstraintHelper] = {ctc: ConstraintHelper(ctc) for ctc in self._fm.get_constraints()}
 
     @property
     def attributes(self) -> dict[Feature, list[Attribute]]:
@@ -34,89 +35,48 @@ class FM(FeatureModel):
             self._attributes[feature] = attributes
 
     def get_constraints(self) -> list[Constraint]:
-        return self._fm.get_constraints()
+        return list(self._constraints.keys())
 
     def get_simple_constraints(self) -> list[Constraint]:
-        return [ctc for ctc in self.get_constraints() if ctc.is_simple_constraint()]
+        return [ctc for ctc in self._constraints if self._constraints[ctc].is_simple_constraint()]
 
     def get_complex_constraints(self) -> list[Constraint]:
-        return [ctc for ctc in self.get_constraints() if ctc.is_complex_constraint()]
+        return [ctc for ctc in self._constraints if self._constraints[ctc].is_complex_constraint()]
 
     def get_requires_constraints(self) -> list[Constraint]:
-        return [ctc for ctc in self.get_constraints() if ctc.is_requires_constraint()]
+        return [ctc for ctc in self._constraints if self._constraints[ctc].is_requires_constraint()]
 
     def get_excludes_constraints(self) -> list[Constraint]:
-        return [ctc for ctc in self.get_constraints() if ctc.is_excludes_constraint()]
+        return [ctc for ctc in self._constraints if self._constraints[ctc].is_excludes_constraint()]
 
     def get_pseudocomplex_constraints(self) -> list[Constraint]:
-        return [ctc for ctc in self.get_constraints() if ctc.is_pseudocomplex_constraint()]
+        return [ctc for ctc in self._constraints if self._constraints[ctc].is_pseudocomplex_constraint()]
 
     def get_strictcomplex_constraints(self) -> list[Constraint]:
-        return [ctc for ctc in self.get_constraints() if ctc.is_strictcomplex_constraint()]
+        return [ctc for ctc in self._constraints if self._constraints[ctc].is_strictcomplex_constraint()]
 
 
 
 class ConstraintHelper():
 
     def __init__(self, constraint: Constraint) -> None:
-        self._ctc = constraint
+        self._constraint = constraint
+        self._formulas = fm_utils.split_constraint(constraint)
 
     def is_simple_constraint(self) -> bool:
         return self.is_requires_constraint() or self.is_excludes_constraint()
 
     def is_complex_constraint(self) -> bool:
-        return self.is_pseudocomplex_constraint() or self.is_strictcomplex_constraint()
+        return not self.is_simple_constraint()
 
     def is_requires_constraint(self) -> bool:
-        root_op = self._ctc.ast.root
-        if root_op.is_binary_op():
-            if root_op.data in [ASTOperation.REQUIRES, ASTOperation.IMPLIES]:
-                return root_op.left.is_term() and root_op.right.is_term()
-            elif root_op.data == ASTOperation.OR:
-                neg_left = root_op.left.data == ASTOperation.NOT and root_op.left.left.is_term()
-                neg_right = root_op.right.data == ASTOperation.NOT and root_op.right.left.is_term()
-                return neg_left and root_op.right.is_term() or neg_right and root_op.left.is_term()
-        return False
+        return fm_utils.is_requires_constraint(self._constraint)
 
     def is_excludes_constraint(self) -> bool:
-        root_op = self._ctc.ast.root
-        if root_op.is_binary_op():
-            if root_op.data in [ASTOperation.EXCLUDES, ASTOperation.XOR]:
-                return root_op.left.is_term() and root_op.right.is_term()
-            elif root_op.data in [ASTOperation.REQUIRES, ASTOperation.IMPLIES]:
-                neg_right = root_op.right.data == ASTOperation.NOT and root_op.right.left.is_term()
-                return root_op.left.is_term() and neg_right
-            elif root_op.data == ASTOperation.OR:
-                neg_left = root_op.left.data == ASTOperation.NOT and root_op.left.left.is_term()
-                neg_right = root_op.right.data == ASTOperation.NOT and root_op.right.left.is_term()
-                return neg_left and neg_right
-        return False
+        return fm_utils.is_excludes_constraint(self._constraint)
 
     def is_strictcomplex_constraint(self) -> bool:
-        if len(self._clauses) == 1 and len(self._clauses[0]) == 2:
-            nof_negative_clauses = sum(var.startswith('-') for var in self._clauses[0])
-            return nof_negative_clauses == 0
-        strictcomplex = False
-        i = iter(self._clauses)
-        while not strictcomplex and (cls := next(i, None)) is not None:
-            if len(cls) != 2:
-                strictcomplex = True
-            else:
-                nof_negative_clauses = sum(var.startswith('-') for var in cls)
-                if nof_negative_clauses not in [1, 2]:
-                    strictcomplex = True
-        return strictcomplex
+        return any(not fm_utils.is_simple_constraint(ctc) for ctc in self._formulas)
 
     def is_pseudocomplex_constraint(self) -> bool:
-        if len(self._clauses) == 1:
-            return False
-        strictcomplex = False
-        i = iter(self._clauses)
-        while not strictcomplex and (cls := next(i, None)) is not None:
-            if len(cls) != 2:
-                strictcomplex = True
-            else:
-                nof_negative_clauses = sum(var.startswith('-') for var in cls)
-                if nof_negative_clauses not in [1, 2]:
-                    strictcomplex = True
-        return not strictcomplex
+        return len(self._formulas) > 1 and all(fm_utils.is_simple_constraint(ctc) for ctc in self._formulas)
--- rhea-backend/rhea/flamapy/metamodels/fm_metamodel/models/fm_utils.py
+++ rhea-backend/rhea/flamapy/metamodels/fm_metamodel/models/fm_utils.py
@@ -2,6 +2,7 @@ from typing import Any
 import datetime
 
 from famapy.core.models import AST, ASTOperation
+from famapy.core.models import ast
 from famapy.metamodels.fm_metamodel.models import Constraint
 
 from rhea.flamapy.metamodels.fm_metamodel.models import Type
@@ -24,14 +25,62 @@ def parse_type_value(value: str) -> tuple[Type, Any]:
     return (Type('String'), value)
 
 
-def split_ast(formula: AST) -> list[AST]:
+def split_constraint(constraint: Constraint) -> list[Constraint]:
+    """Given a constraint, split it in multiple constraints separated by the AND operator."""
+    asts = split_formula(constraint.ast)
+    asts_nnf = [ast.convert_into_nnf(ctc) for ctc in asts]
+    asts = []
+    for ctc in asts_nnf:
+        asts.extend(split_formula(ctc))
+    asts_cnf = [ast.convert_into_cnf(ctc) for ctc in asts]
+    asts = []
+    for ctc in asts_cnf:
+        asts.extend(split_formula(ctc))
+    return [Constraint(f'{constraint.name}{i}', ast) for i, ast in enumerate(asts)]
+
+
+def split_formula(formula: AST) -> list[AST]:
     """Given a formula, returns a list of formulas separated by the AND operator."""
-    print(f'AST: {formula.pretty_str()}')
     res = []
     node = formula.root
     if node.data == ASTOperation.AND:
-        res.extend(split_ast(AST(node.left)))
-        res.extend(split_ast(AST(node.right)))
+        res.extend(split_formula(AST(node.left)))
+        res.extend(split_formula(AST(node.right)))
     else:
         res.append(formula)
     return res
+
+
+def is_simple_constraint(constraint: Constraint) -> bool:
+    return is_requires_constraint(constraint) or is_excludes_constraint(constraint)
+
+
+def is_complex_constraint(constraint: Constraint) -> bool:
+    return not is_simple_constraint(constraint)
+
+
+def is_requires_constraint(constraint: Constraint) -> bool:
+    root_op = constraint.ast.root
+    if root_op.is_binary_op():
+        if root_op.data in [ASTOperation.REQUIRES, ASTOperation.IMPLIES]:
+            return root_op.left.is_term() and root_op.right.is_term()
+        elif root_op.data == ASTOperation.OR:
+            neg_left = root_op.left.data == ASTOperation.NOT and root_op.left.left.is_term()
+            neg_right = root_op.right.data == ASTOperation.NOT and root_op.right.left.is_term()
+            return neg_left and root_op.right.is_term() or neg_right and root_op.left.is_term()
+    return False
+
+
+def is_excludes_constraint(constraint: Constraint) -> bool:
+    root_op = constraint.ast.root
+    if root_op.is_binary_op():
+        if root_op.data in [ASTOperation.EXCLUDES, ASTOperation.XOR]:
+            return root_op.left.is_term() and root_op.right.is_term()
+        elif root_op.data in [ASTOperation.REQUIRES, ASTOperation.IMPLIES]:
+            neg_right = root_op.right.data == ASTOperation.NOT and root_op.right.left.is_term()
+            return root_op.left.is_term() and neg_right
+        elif root_op.data == ASTOperation.OR:
+            neg_left = root_op.left.data == ASTOperation.NOT and root_op.left.left.is_term()
+            neg_right = root_op.right.data == ASTOperation.NOT and root_op.right.left.is_term()
+            return neg_left and neg_right
+    return False
