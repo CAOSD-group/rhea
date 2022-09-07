72f438ff8cfd5298c3b950f1761357e6b40c3277
José Miguel Horcas <horcas@uma.es>
2022-09-07 16:31:56 +0200

    improve code for elimination of complex

--- rhea-backend/rhea/refactorings/elimination_complex_constraints.py
+++ rhea-backend/rhea/refactorings/elimination_complex_constraints.py
@@ -9,6 +9,7 @@ from flamapy.core.models.ast import AST, ASTOperation, Node
 from rhea.metamodels.fm_metamodel.models import FM, ConstraintHelper
 from rhea.refactorings import FMRefactoring
 from rhea.refactorings import utils
+from rhea.metamodels.fm_metamodel.models import fm_utils
 
 
 class EliminationComplexConstraints(FMRefactoring):
@@ -19,62 +20,50 @@ class EliminationComplexConstraints(FMRefactoring):
 
     @staticmethod
     def get_instances(model: FeatureModel) -> list[Constraint]:
-        return [ctc for ctc in model.get_constraints() if ConstraintHelper(ctc).is_complex_constraint()]
+        return [ctc for ctc in model.get_constraints() if fm_utils.is_complex_constraint(ctc)]
 
     @staticmethod
     def transform(model: FeatureModel, instance: Constraint) -> FeatureModel:
         if instance is None:
-            raise Exception(f'There is not constraint with name "{str(instance)}".')
-        if not ConstraintHelper(instance).is_complex_constraint():
-            raise Exception(f'Operator {str(instance)} is not complex constraint.')
+            raise Exception(f'Constraint {instance} is None.')
+        if not fm_utils.is_complex_constraint(instance):
+            raise Exception(f'Constraint {instance} is not complex.')
 
-        # print(f'MODELO: {model}')
-
-        print(f'Constraint: {str(instance)}')
-        # print(f'Constraint AST: {str(instance.ast)}')
-        # print(f'Constraint AST.root: {str(instance.ast.root)}')
-        print(f'datos del nodo izquierdo: {instance.ast.root.left.data}')
-        print(f'datos del nodo derecho: {instance.ast.root.right}')
-        
         new_or = Feature(utils.get_new_feature_name(model, 'OR'), is_abstract=True)
-
-        features_names = instance.get_features()  # string
-        features_new_names = []
-        dict_constraint = get_feature_clause(instance)  # NOT before negatives (dict)
-        print(f'NEW CONSTRAINT: {dict_constraint}')
-        for i, f in enumerate(features_names):
+        features = []
+        dict_constraint = get_features_clauses(instance)  # NOT before negatives (dict)
+        print(dict_constraint)
+        for i, f in enumerate(dict_constraint.keys()):
             new_feature = Feature(utils.get_new_feature_name(model, f), parent=new_or, is_abstract=True)
-            features_new_names.append(new_feature)
-            if not dict_constraint[f]:
-                ast_operation = ASTOperation.EXCLUDES
-            else:
-                ast_operation = ASTOperation.IMPLIES
-            ctc = Constraint(f'CTC({i})', AST.create_binary_operation(ast_operation,
-                                                                Node(new_feature.name), Node(f)))
-            print(f'CONSTRAINT: {ctc}')
+            features.append(new_feature)
+            ast_operation = ASTOperation.REQUIRES if dict_constraint[f] else ASTOperation.EXCLUDES
+            node_left = Node(f) if dict_constraint[f] else Node(ASTOperation.NOT, Node(f))
+            #ctc = Constraint(f'CTC{i}', AST.create_binary_operation(ASTOperation.REQUIRES,
+            #                 Node(new_feature.name), node_left))
+            ctc = Constraint(f'CTC{i}', AST.create_binary_operation(ast_operation,
+                             Node(new_feature.name), Node(f)))
             model.ctcs.append(ctc)
-
 
         model.ctcs.remove(instance)
 
         # New branch with OR as root
-        rel_or = Relation(new_or, features_new_names, 1, len(features_new_names))  # OR
-        print(f'FEATURES: {[f.name for f in features_new_names]}')
-        print(f'REL OR: {str(rel_or)}')
+        rel_or = Relation(new_or, features, 1, len(features))  # OR
         new_or.add_relation(rel_or)
         
         # New root
         new_root = Feature(utils.get_new_feature_name(model, 'root'), is_abstract=True)
         rel_1 = Relation(new_root, [model.root], 1, 1)  # mandatory
         new_root.add_relation(rel_1)
+        model.root.parent = new_root
         rel_2 = Relation(new_root, [new_or], 1, 1)  # mandatory
         new_root.add_relation(rel_2)
+        new_or.parent = new_root
         model.root = new_root
         
         return model
 
 
-def get_feature_clause(instance: Constraint) -> dict:
+def get_features_clauses(instance: Constraint) -> dict:
     """Returns a dictionary of 'Features -> bool',
     that sets 'bool' to FALSE if the feature has a negation"""
     features = {}
