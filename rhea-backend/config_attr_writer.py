968e18dd346acb3922822da925838aecc6e861d2
Adriana <anhe2p9@uma.es>
2022-09-14 14:51:14 +0200

    Configuration attribute writer

--- rhea-backend/config_attr_writer.py
+++ rhea-backend/config_attr_writer.py
@@ -1,5 +1,6 @@
 from flamapy.core.transformations import ModelToText
-from typing import Any 
+from typing import Any
+import random
 
 from flamapy.metamodels.pysat_metamodel.transformations import FmToPysat
 from flamapy.metamodels.pysat_metamodel.operations import (
@@ -9,8 +10,7 @@ from flamapy.metamodels.pysat_metamodel.operations import (
 
 from flamapy.metamodels.fm_metamodel.models import FeatureModel, Feature, Relation, Constraint
 
-
-CATEGORY_THEORY_TEMPLATE = 'category_theory_template.cql'
+from rhea.refactorings import utils
 
 
 class ConfigurationAttributeWriter(ModelToText):
@@ -31,13 +31,14 @@ class ConfigurationAttributeWriter(ModelToText):
         self.attributes = attributes
 
     def transform(self) -> str:
-        ca_str = configurations_to_csv(self.source_model)
+        ca_str = configurations_to_csv(self.source_model, random.randint(1, 5))
         with open(self.path, 'w', encoding='utf-8') as file:
             file.write(ca_str)
         return ca_str
 
 
-def configurations_to_csv(feature_model: FeatureModel) -> str:
+def configurations_to_csv(feature_model: FeatureModel, n_attr: int) -> str:
+    # Transform the configurations to csv
     sat_model = FmToPysat(feature_model).transform()
     configurations = Glucose3Products().execute(sat_model).get_result()
     print(f'number of configurations: {len(configurations)}')
@@ -45,9 +46,19 @@ def configurations_to_csv(feature_model: FeatureModel) -> str:
     features_list = []
     features_list = feature_model.get_features()
     header = ','.join((f.name for f in features_list))
+
+    header_attr = []
+    for i in range(n_attr):
+        header_attr.append(utils.get_new_attr_name(header_attr, 'Attribute'))
+    header_attr = ','.join(header_attr)
+    print(f'header attribute: {header_attr}, header attribute type: {type(header_attr)}')
+    if len(header_attr)>=1:
+        header = header + ',' + header_attr
+
     result = f'{header}'
     for c in configurations:
         row = ','.join((str(f.name in c) for f in features_list))
+        if len(header_attr)>=1:
+            row = row + ',' + ','.join([str(random.randint(0, 10)) for i in range(n_attr)])
         result += f'\n{row}'
-    #feature_model.root.get_attributes()
     return result
--- rhea-backend/main_conf_attr.py
+++ rhea-backend/main_conf_attr.py
@@ -25,7 +25,7 @@ from rhea.refactorings import utils
 
 
 MODEL_PATH = 'tests/models/general_models/Pizzas.uvl'
-n_attr = 3
+n_attr = random.randint(1, 5)
 
 
 def main(fm_path: str, n_attributes: int):
@@ -33,7 +33,7 @@ def main(fm_path: str, n_attributes: int):
     fm_basename = os.path.basename(fm_path)
     fm_dirname = os.path.dirname(fm_path)
     fm_name = fm_basename[:fm_basename.find('.')]  # Remove extension
-    output_path = os.path.join('rhea-backend/tests/config_output', fm_name + ConfigurationAttributeWriter.get_destination_extension())
+    output_path = os.path.join('tests/config_output', fm_name + ConfigurationAttributeWriter.get_destination_extension())
     
     # Load the feature model
     if MODEL_PATH.endswith('.gfm.json'):
@@ -45,7 +45,8 @@ def main(fm_path: str, n_attributes: int):
     else:
         raise Exception(f'Error, invalid model {MODEL_PATH}.')
 
-    ct_str = generate_configurations(fm, n_attr)
+    # ct_str = generate_configurations(fm, n_attributes)
+    ct_str = ConfigurationAttributeWriter(path=output_path, source_model=fm).transform()
 
     # Print the result (optional)
     print(ct_str)
@@ -54,33 +55,25 @@ def generate_configurations(fm: FeatureModel, n_attr: int) -> str:
     # Transform the configurations to csv
     sat_model = FmToPysat(fm).transform()
     configurations = Glucose3Products().execute(sat_model).get_result()
-    print(f'number of configurations: {len(configurations)}')
 
     features_list = []
     features_list = fm.get_features()
     header = ','.join((f.name for f in features_list))
 
     header_attr = []
-    attributes = []
     for i in range(n_attr):
-        print(i)
         header_attr.append(utils.get_new_attr_name(header_attr, 'Attribute'))
-        attributes.append(str(random.randint(0, 10)))
     header_attr = ','.join(header_attr)
-    attr = ','.join(attributes)
-
-    print(f'HEADER + ATTRIBUTES: {header+attr}')
-    header = header + ',' + header_attr
+    if len(header_attr)>=1:
+        header = header + ',' + header_attr
 
     result = f'{header}'
     for c in configurations:
         row = ','.join((str(f.name in c) for f in features_list))
+        if len(header_attr)>=1:
+            row = row + ',' + ','.join([str(random.randint(0, 10)) for i in range(n_attr)])
         result += f'\n{row}'
     
-    #feature_model.root.get_attributes()
-    # random.randint(0, 10)
-    # random.random()
-    # random.randint(0,1)
     return result
     
 
