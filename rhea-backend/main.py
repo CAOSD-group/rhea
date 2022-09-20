a2e6cb2d69b1a11444d8476d83a6b4acc06d46b5
Adriana <anhe2p9@uma.es>
2022-09-20 14:23:29 +0200

    Prepare automated tests

--- rhea-backend/main.py
+++ rhea-backend/main.py
@@ -39,7 +39,8 @@ REFACTORING_REQUIRES = EliminationSimpleConstraintsRequires
 REFACTORING_EXCLUDES = EliminationSimpleConstraintsExcludes
 REFACTORING_REQUIRES_WITHOUT_DICT = EliminationSimpleConstraintsRequiresWithoutDict
 REFACTORING_EXCLUDES_WIHOUT_DICT = EliminationSimpleConstraintsExcludesWithoutDict
-MODEL_PATH = 'tests/models/split_constraints/input_models/JHipster.uvl'
+#MODEL_PATH = 'tests/models/split_constraints/input_models/Pizza.uvl'
+MODEL_PATH = 'tests/models/real_models/input_models/Pizzas.uvl'
 OUTPUT_PATH = 'output.uvl'
 OUTPUT_CONSOLE = 'output.txt'
 ##################################################################################################
