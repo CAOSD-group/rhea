5c6592e1b8200ba47d7b5ff95d221b6a5bf01c42
Enriquelp <enriquelopezencinas@gmail.com>
2024-01-25 15:41:25 +0100

    Desarrollo de featureide_writer.py para la libreria flammapy

--- rhea-backend/prueba_fide_writer.py
+++ rhea-backend/prueba_fide_writer.py
@@ -2,11 +2,10 @@ from email.base64mime import body_decode
 import os
 from typing import Any 
 
-from flamapy.metamodels.fm_metamodel.transformations import UVLReader, UVLWriter, FeatureIDEReader, GlencoeReader
+from flamapy.metamodels.fm_metamodel.transformations import UVLReader, UVLWriter, FeatureIDEReader, GlencoeReader, XMLReader
 from flamapy.metamodels.pysat_metamodel.transformations import FmToPysat
-from flamapy.metamodels.pysat_metamodel.operations import (
-    Glucose3ProductsNumber,
-    Glucose3Products
+from flamapy.metamodels.bdd_metamodel.operations import (
+    BDDProductsNumber
 )
 
 from flamapy.metamodels.fm_metamodel.operations import FMEstimatedProductsNumber
@@ -18,12 +17,30 @@ from flamapy.metamodels.fm_metamodel.models import FeatureModel
 from rhea.metamodels.fm_metamodel.models import fm_utils
 from rhea.metamodels.fm_metamodel.transformations.featureide_writer import FeatureIDEWriter
 
+
+
 MODEL_PATH = 'fm_models/Pizzas.uvl'
+MODEL_PATH2 = 'fm_models/JHipster.uvl'
 
 def main():
-    fm = UVLReader(MODEL_PATH).transform()
+    model_path = MODEL_PATH2
+ 
+    fm = UVLReader(model_path).transform()
+    bdd_model = FmToBDD(fm).transform()
+    n_products = BDDProductsNumber().execute(bdd_model).get_result()
+    print(f'#Products (UVL): {n_products}')
 
-    FeatureIDEWriter('Pizzas.xml', fm).transform()
+    FeatureIDEWriter('pruebaFeatureIDE.xml', fm).transform()
+    fm_fide = FeatureIDEReader('pruebaFeatureIDE.xml').transform()
+    if fm == fm_fide:
+        print(f'Equals.')
+    else:
+        print(f'Differents.')
+
+    bdd_model_fide = FmToBDD(fm_fide).transform()
+    n_products = BDDProductsNumber().execute(bdd_model_fide).get_result()
+    print(f'#Products (FIDE): {n_products}')
+
 
 if __name__ == '__main__':
     main()
--- rhea-backend/rhea/metamodels/fm_metamodel/transformations/featureide_writer.py
+++ rhea-backend/rhea/metamodels/fm_metamodel/transformations/featureide_writer.py
@@ -1,4 +1,4 @@
-from xml.etree import ElementTree
+from xml.etree import ElementTree as ET
 from xml.etree.ElementTree import Element
 import inspect
 import importlib
@@ -10,7 +10,7 @@ from enum import Enum
 from flamapy.core.models.ast import Node, ASTOperation
 from flamapy.core.transformations import ModelToText
 
-from flamapy.metamodels.fm_metamodel.models import FeatureModel, Feature, Constraint, Attribute
+from flamapy.metamodels.fm_metamodel.models import FeatureModel, Feature, Constraint, Attribute, Relation
 
 from rhea import refactorings
 from rhea import language_constructs
@@ -20,15 +20,15 @@ from rhea.fm_characterization import FMAnalysis
 
 class FeatureIDEWriter(ModelToText):
 
-    CTC_TYPES = {ASTOperation.NOT: 'NotTerm',
-                 ASTOperation.AND: 'AndTerm',
-                 ASTOperation.OR: 'OrTerm',
-                 ASTOperation.XOR: 'disj',
-                 ASTOperation.IMPLIES: 'ImpliesTerm',
-                 ASTOperation.REQUIRES: 'Impl',
-                 ASTOperation.EXCLUDES: 'ExcludesTerm',
-                 ASTOperation.EQUIVALENCE: 'EquivalentTerm',
-                 'FEATURE': 'FeatureTerm'}
+    CTC_TYPES = {ASTOperation.NOT: 'not',
+                 ASTOperation.AND: 'conj',
+                 ASTOperation.OR: 'disj',
+                 ASTOperation.XOR: 'alt',
+                 ASTOperation.IMPLIES: 'imp',
+                 ASTOperation.REQUIRES: 'imp',
+                 ASTOperation.EXCLUDES: 'impn',
+                 ASTOperation.EQUIVALENCE: 'eq',
+                 'FEATURE': 'Feature'}
 
     @staticmethod
     def get_destination_extension() -> str:
@@ -39,77 +39,74 @@ class FeatureIDEWriter(ModelToText):
         self.source_model = source_model
 
     def transform(self) -> str:
-        xml_str = _to_featureidexml(self.source_model)
-        if self.path is not None:
-            with open(self.path, 'w', encoding='utf8') as file:
-                file.write(xml_str, file, indent=4)
-        return xml_str
+        return _to_featureidexml(self.source_model).write(self.path)
+        # xml_str = _to_featureidexml(self.source_model).__str__()
+        # print(xml_str)
+        # if self.path is not None:
+        #     with open(self.path, 'w', encoding='utf8') as file:
+        #         file.write(xml_str,indent=4)
+        # return xml_str
 
 
-def _to_featureidexml(feature_model: FeatureModel) -> dict[str, Any]:
-    # for relation in feature_model.root.get_relations():
-
-        
-    result: dict[str, Any] = {}
-    result['name'] = f'FM_{feature_model.root.name}'
-    result['hash'] = str(hash(feature_model))
-    result['features'] = _get_tree_info(feature_model.root)
-    result['constraints'] = _get_constraints_info(feature_model.get_constraints())
-    result['refactorings'] = _get_refactorings_info(feature_model)
-    result['language_constructs'] = _get_language_constructs_info(feature_model)
-    result['semantics_metrics'] = _get_semantics_metrics(feature_model)
-    result['tools_info'] = _get_tools_info()
-    return result
+def _to_featureidexml(feature_model: FeatureModel):
+    featureModel = ET.Element("featureModel")
+    tree = ET.ElementTree(featureModel)
+    struct = ET.SubElement(featureModel, "struct")
+    constraints = ET.SubElement(featureModel, "constraints")
+    root = ET.SubElement(struct, _tag_element(feature_model.root), _get_attributes(feature_model.root))
+    _create_tree(root, feature_model.root.get_relations())
+    _get_constraints(constraints, feature_model.get_constraints())
+    return tree
 
 
-def _get_tree_info(feature: Feature) -> dict[str, Any]:
-    feature_info = {}
-    feature_info['name'] = feature.name
-    feature_info['abstract'] = feature.is_abstract
-
-    relations = []
-    for relation in feature.get_relations():
-        relation_info = {}
-        relation_type = JSONFeatureType.FEATURE.value
-        if relation.is_alternative():
-            relation_type = JSONFeatureType.XOR.value
-        elif relation.is_or():
-            relation_type = JSONFeatureType.OR.value
-        elif relation.is_mutex():
-            relation_type = JSONFeatureType.MUTEX.value
-        elif relation.is_cardinal():
-            relation_type = JSONFeatureType.CARDINALITY.value
-        elif relation.is_mandatory():
-            relation_type = JSONFeatureType.MANDATORY.value
-        elif relation.is_optional():
-            relation_type = JSONFeatureType.OPTIONAL.value
-        relation_info['type'] = relation_type
-        relation_info['card_min'] = relation.card_min
-        relation_info['card_max'] = relation.card_max
-        children = []
-        for child in relation.children:
-            children.append(_get_tree_info(child))
-        relation_info['children'] = children
-        relations.append(relation_info)
-
-    feature_info['relations'] = relations
-
-    # Attributes
-    feature_info['attributes'] = _get_attributes_info(feature.get_attributes())
-    return feature_info
+def _create_tree(parentElement: Element, relations: list[Relation]):
+    for rel in relations:
+        for child in rel.children:
+            newElem = ET.SubElement(parentElement, _tag_element(child), _get_attributes(child))
+            _create_tree(newElem, child.get_relations())
 
 
-def _get_attributes_info(attributes: list[Attribute]) -> list[dict[str, Any]]:
-    attributes_info = []
-    for attribute in attributes:
-        attr_info = {}
-        attr_info['name'] = attribute.name
-        if attribute.default_value is not None:
-            attr_info['value'] = attribute.default_value
-        attributes_info.append(attr_info)
-    return attributes_info
+def _get_attributes(feature: Feature):
+    atributes = {}
+    if feature.is_mandatory and not feature.is_leaf(): atributes['mandatory'] ='true'
+    atributes['name'] = feature.name
+    return atributes
+ 
+
+def _tag_element(feature :Feature):
+    name = ''
+    if feature.is_leaf(): name = 'feature'
+    else: 
+        if feature.is_or_group(): name = 'or'
+        elif feature.is_alternative_group(): name = 'alt'
+        else: name = 'and'
+    return name
 
 
+def _get_constraints(parentElement: Element, constraints: list[Constraint]):
+    dictConstraints = _get_constraints_info(constraints)
+    for const in dictConstraints:
+        rule = ET.SubElement(parentElement, 'rule')
+        newConstraint = ET.SubElement(rule, const['ast']['type'])
+        for operand in const['ast']['operands']:
+            _create_elem_constraint(operand, newConstraint)
+            
+
+def _create_elem_constraint(operand, parentElement: Element):
+    if operand['type'] == 'Feature':
+        elem = ET.SubElement(parentElement, 'var')
+        elem.text = operand['operands'][0]
+    elif operand['type'] == 'not':
+        elem = ET.SubElement(parentElement, 'not')
+    elif operand['type'] == 'conj':
+        elem = ET.SubElement(parentElement, 'conj')
+    elif operand['type'] == 'disj':
+         elem = ET.SubElement(parentElement, 'disj')
+    if len(operand['operands'])>1 or operand['type'] == 'not':
+        for op in operand['operands']:
+            _create_elem_constraint(op, elem)            
+
+ 
 def _get_constraints_info(constraints: list[Constraint]) -> list[dict[str, Any]]:
     constraints_info = []
     for ctc in constraints:
@@ -124,10 +121,11 @@ def _get_constraints_info(constraints: list[Constraint]) -> list[dict[str, Any]]
 def _get_ctc_info(ast_node: Node) -> dict[str, Any]:
     ctc_info: dict[str, Any] = {}
     if ast_node.is_term():
-        ctc_info['type'] = JSONWriter.CTC_TYPES['FEATURE']
+        ctc_info['type'] = FeatureIDEWriter.CTC_TYPES['FEATURE']
         ctc_info['operands'] = [ast_node.data]
     else:
-        ctc_info['type'] = JSONWriter.CTC_TYPES[ast_node.data]
+        ctc_info['type'] = FeatureIDEWriter.CTC_TYPES[ast_node.data]
+        # if ast_node.data si es EXCLUDES entonces hay que crear dos nodos, un impl y un negado en la derecha.
         operands = []
         left = _get_ctc_info(ast_node.left)
         operands.append(left)
@@ -136,86 +134,3 @@ def _get_ctc_info(ast_node: Node) -> dict[str, Any]:
             operands.append(right)
         ctc_info['operands'] = operands
     return ctc_info
-
-
-def _get_refactorings_info(feature_model: FeatureModel) -> list[dict[str, Any]]:
-    # Get class names
-    class_list = list(refactorings.__all__)
-    class_list.remove('FMRefactoring')
-
-    # Get modules and class objects
-    modules = inspect.getmembers(refactorings)
-    modules = filter(lambda x: inspect.ismodule(x[1]), modules)
-    modules = [importlib.import_module(m[1].__name__) for m in modules]
-    classes = [getattr(m, c) for m in modules for c in class_list if hasattr(m, c)]
-
-    refactorings_info = []
-    for class_ in classes:
-        ref_info = {}
-        ref_info['id'] = class_.__name__
-        ref_info['name'] = class_.get_name()
-        ref_info['description'] = class_.get_description()
-        ref_info['type'] = class_.get_language_construct_name()
-        ref_info['instances'] = [i.name for i in class_.get_instances(feature_model)]
-        ref_info['applicable'] = class_.is_applicable(feature_model)
-        refactorings_info.append(ref_info)
-    return refactorings_info
-
-
-def _get_language_constructs_info(feature_model: FeatureModel) -> list[dict[str, Any]]:
-    # Get class names
-    class_list = list(language_constructs.__all__)
-    class_list.remove('LanguageConstruct')
-
-    # Get modules and class objects
-    modules = inspect.getmembers(language_constructs)
-    modules = filter(lambda x: inspect.ismodule(x[1]), modules)
-    modules = [importlib.import_module(m[1].__name__) for m in modules]
-    classes = [getattr(m, c) for c in class_list for m in modules if hasattr(m, c)]
-
-    language_constructs_info = []
-    tools = fm_tool_info.get_tools_info()
-    for class_ in classes:
-        lc_info = {}
-        lc_info['id'] = class_.__name__
-        lc_info['name'] = class_.name()
-        lc_info['value'] = len(class_.get_instances(feature_model))
-        lc_info['refactorings'] = [r.__name__ for r in class_.get_refactorings()]
-        tool_support = []
-        for t in tools:
-            for lc in t.support:
-                if lc.__name__ == lc_info['id']:
-                    tool_support.append(t.name)
-        lc_info['tools'] = tool_support
-        language_constructs_info.append(lc_info)
-    return language_constructs_info
-
-
-def _get_semantics_metrics(feature_model: FeatureModel) -> list[dict[str, Any]]:
-    metrics_info = []
-    fm_analysis = FMAnalysis(feature_model)
-    for property in fm_analysis.get_analysis():
-        metric = {}
-        metric['name'] = property.property.name
-        metric['description'] = property.property.description
-        metric['value'] = str(property.value) if property.size is None else str(property.size)
-        metrics_info.append(metric)
-    return metrics_info
-    
-
-def _get_metric(name: str, description: str, value: Any) -> dict[str, Any]:
-    metric = {}
-    metric['name'] = name
-    metric['description'] = description
-    metric[value] = value
-    return metric
-
-
-def _get_tools_info() -> list[dict[str, Any]]:
-    tools_info = []
-    for tool in fm_tool_info.get_tools_info():
-        info = {}
-        info['name'] = tool.name
-        info['extension'] = tool.extension
-        tools_info.append(info)
-    return tools_info
