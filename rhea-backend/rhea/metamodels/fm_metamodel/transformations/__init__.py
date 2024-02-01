3cba49e58493c07bcc23c90b1bb7ff74fe2e65ce
Enriquelp <enriquelopezencinas@gmail.com>
2024-02-01 12:48:55 +0100

    Añadido writter de Clafer

--- rhea-backend/rhea/metamodels/fm_metamodel/transformations/__init__.py
+++ rhea-backend/rhea/metamodels/fm_metamodel/transformations/__init__.py
@@ -2,9 +2,11 @@ from .glencoe_reader import GlencoeReader
 from .json_reader import JSONReader
 from .json_writer import JSONWriter
 from .featureide_writer import FeatureIDEWriter
+from .clafer_writter import ClaferWriter
 
 
 __all__ = ['GlencoeReader',
            'JSONReader',
            'JSONWriter',
-           'FeatureIDEWriter']
+           'FeatureIDEWriter'
+           'ClaferWriter']
